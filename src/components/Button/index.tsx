1d7dd895eb36fa66a884e16ac859fbb857e346b1
volodymyr.zabelskyi <vovcou1999@gmail.com>
2023-10-29 09:52:41 +0200

    init

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export interface ButtonProps {
+    label: string;
+}
+export const Button: React.FC<ButtonProps> = (props) => {
+    return <button>{props.label}</button>;
+};
